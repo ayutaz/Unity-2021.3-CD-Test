c2976f38b45c46acb367413521ef2b17c6da3e4f
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-26 18:54:51 +0900

    feat: ボタンを押すと数字が増えていく簡易機能の追加

---
+++ Assets/_Project/Scripts/CountUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project
+{
+    public class CountUp : MonoBehaviour
+    {
+        [SerializeField] private Button countUpButton;
+        [SerializeField] private TextMeshProUGUI countUpText;
+        private int _countValue = 0;
+
+        private void Awake()
+        {
+            countUpText.text = _countValue.ToString();
+        }
+
+        private void Start()
+        {
+            countUpButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    _countValue++;
+                    countUpText.text = _countValue.ToString();
+                }).AddTo(this);
+        }
+    }
+}
